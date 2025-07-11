969d71a42ca12ee06a73909a8e4f98352448bc86
MariaGuerreroR <mypersonalblog255@gmail.com>
2025-07-10 19:01:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['https://blog-frontend-875r.vercel.app'],
+  origin: [''],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
